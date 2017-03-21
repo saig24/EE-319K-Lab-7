d9c2689dfa8e389efad7a519c2e2f076a9e1e892
Kamyar Mirzazad Barijough <kammirzazad@users.noreply.github.com>
2017-03-21 08:42:27 -0500

    Populated repo from zip file

---
+++ IO.c
@@ -0,0 +1,40 @@
+// IO.c
+// This software configures the switch and LED
+// You are allowed to use any switch and any LED, 
+// although the Lab suggests the SW1 switch PF4 and Red LED PF1
+// Runs on LM4F120 or TM4C123
+// Program written by: put your names here
+// Date Created: 
+// Last Modified:  
+// Lab number: 6
+
+
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+
+//------------IO_Init------------
+// Initialize GPIO Port for a switch and an LED
+// Input: none
+// Output: none
+void IO_Init(void) {
+ // --UUU-- Code to initialize PF4 and PF2
+}
+
+//------------IO_HeartBeat------------
+// Toggle the output state of the  LED.
+// Input: none
+// Output: none
+void IO_HeartBeat(void) {
+ // --UUU-- PF2 is heartbeat
+}
+
+
+//------------IO_Touch------------
+// wait for release and press of the switch
+// Delay to debounce the switch
+// Input: none
+// Output: none
+void IO_Touch(void) {
+ // --UUU-- wait for release; delay for 20ms; and then wait for press
+}  
+
